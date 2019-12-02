8ed3c18d63fe073f658bf812584834d768f02dd1
Mallikarjuna <harish.mallikarjuna@accenture.com>
2019-12-02 13:42:33 +0530

    fix

--- push.js
+++ push.js
@@ -7,6 +7,6 @@ let vapidKeys = {
 
 push.setVapidDetails('mailto:test@code.com', vapidKeys.publicKey, vapidKeys.privateKey)
 
-let sub= {};
+let sub= {"endpoint":"https://fcm.googleapis.com/fcm/send/fHzYuuaxitE:APA91bElINMox6d9mgi6WNbN0IJU2DnnzN-gGfHrSeAmSMyhUh3og1X3T-XnkS0t_x_3FhmCYghTn-K6PQLs_AHwW7skWKcKk6SWEf1aEAzJDJXAmGNBD3c_jmHpupvgt8ZBbbS8VvcE","expirationTime":null,"keys":{"p256dh":"BOBDmltgbAIyAGrtS7vjCSkx5om5Md2DSAQ_4mVazcANQ_bqiG7oYpEyz_PVJCN0YYj8Hqepiu3f0Bi_40yBUSw","auth":"l1SExMfK8yjt0fXZrvXILQ"}};
 
 push.sendNotification(sub, 'test message')
